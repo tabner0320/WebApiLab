0c51e845f6341b77278c450a84987627dfc03ef1
Theophilus (Theo) Abner <theophilusabner0204@yahoo.com>
2026-07-02 22:30:14 -0400

    Add Person model and people API endpoint

---
+++ WebApiLab.API/Models/Person.cs
@@ -0,0 +1,3 @@
+namespace WebApiLab.API.Models;
+
+public record Person(string Name, string Language, string Id, string Bio, decimal Version);
--- WebApiLab.API/Program.cs
+++ WebApiLab.API/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using WebApiLab.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+string jsonFile = File.ReadAllText("./Resources/64KB.json");
+
+var jsonData = JsonSerializer.Deserialize<List<Person>>(
+    jsonFile,
+    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,28 +25,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/people", () => jsonData)
+    .WithName("GetPeople")
+    .WithOpenApi()
+    .Produces<List<Person>>(StatusCodes.Status200OK);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
